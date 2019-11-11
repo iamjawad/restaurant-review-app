a46223a67ca18f0f7be549b5984bd3e117d2e1b4
Jawad Ahmed <ijawadak@gmail.com>
2019-11-11 15:02:57 +0500

    Google street view added

--- src/GMap.js
+++ src/GMap.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import APIKey from './APIKey.js'
 import imgRestaurant from './images/restaurant.png';
+import location from './images/location.png';
 import DataContext from './contexts/DataContext';
 import NewRestaurant from './NewRestaurant';
 
@@ -14,6 +15,7 @@ class GMap extends React.Component {
         this.nearbyCallback = this.nearbyCallback.bind(this);
         this.API = new APIKey();
         this.map = null;
+        this.dragged_location = 0;
         this.state = {
             currentLocation: 0,
             newRestaurant:{
@@ -22,8 +24,15 @@ class GMap extends React.Component {
             }
         };
 
+        this.infowindow = "";
+        // this.createMap = this.createMap.bind(this);
         //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY
         
+    }
+
+    getInfowindow(marker) {
+        this.infowindow.setContent('<div style="width:400px; height:400px" id="pano"></div>')
+        this.infowindow.open(this.map, marker);
     }
 
     componentDidMount() {
@@ -39,6 +48,23 @@ class GMap extends React.Component {
 
     }
 
+    setStreetView(coords, id) {
+        let panorama = new window.google.maps.StreetViewPanorama(
+            document.getElementById("pano"), {
+            position: coords,
+            pov: {
+                heading: 34,
+                pitch: 10
+            }
+        });
+
+        if (panorama.getStatus() === undefined) {
+            return false;
+        }
+        console.log("GGGGGGG");
+        this.map.setStreetView(panorama);
+    }
+
     createMap(position) {
         window.clientPosition = position;
         window.gClickCoords = position;
@@ -48,22 +74,28 @@ class GMap extends React.Component {
                                      {zoom: 16, center: position, mapTypeId: window.google.maps.MapTypeId.ROAD}
                                     );
         this.map = window.gMap;
-        let marker = new window.google.maps.Marker({position: position, map: this.map, draggable: true,title: 'You are here!',});
+
+        this.infowindow = new window.google.maps.InfoWindow({
+            content: ''
+        });
+
+        let currentMarker = new window.google.maps.Marker({ position: position, map: this.map, draggable: false, title: 'You are here!',icon: location});
+        let marker = new window.google.maps.Marker({position: position, map: this.map, draggable: true,});
         this.nearbyRestaurants();
         
         let clatLng = position;
-        var cityCircle = new window.google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: window.gMap,
-            center: clatLng,
-            radius: 1000
-          });
-          window.gCircle = cityCircle;
-          cityCircle.setMap(this.map);
+        // var cityCircle = new window.google.maps.Circle({
+        //     strokeColor: '#FF0000',
+        //     strokeOpacity: 0.8,
+        //     strokeWeight: 2,
+        //     fillColor: '#FF0000',
+        //     fillOpacity: 0.35,
+        //     map: window.gMap,
+        //     center: clatLng,
+        //     radius: 1000
+        //   });
+        //   window.gCircle = cityCircle;
+        //   cityCircle.setMap(this.map);
          //Add listener
          window.google.maps.event.addListener(window.gMap, "click", function (event) {
             let latitude = event.latLng.lat();
@@ -75,33 +107,53 @@ class GMap extends React.Component {
             window.gClickCoords = latLng;
         }); //end addListener
 
-        window.google.maps.event.addListener(marker, "dragend", function (event) {
+        window.google.maps.event.addListener(marker, "dragend", (event) => {
             window.gClickCoords = event.latLng;
+            this.nearbyRestaurants(event.latLng);
         });
+
+        // window.google.maps.event.addListener(this.map, 'dragend', function (event) { 
+        //     // this.nearbyRestaurants(event.latLng);
+        //     console.log(event);
+        //  });
+
         this.userReviews();
         this.placeMarkers();
         this.setState({db: this.props.db});
+        
     }
 
     placeMarkers() {
         const db = this.context.restaurants;
-        for (var i = 0; i < db.length; i++) {
-            var coords = db[i];
-            var latLng = new window.google.maps.LatLng(coords.lat,coords.long);
-            var marker = new window.google.maps.Marker({
+        
+        for (let i = 0; i < db.length; i++) {
+            let coords = db[i];
+            let latLng = new window.google.maps.LatLng(coords.lat,coords.long);
+            let marker = new window.google.maps.Marker({
             position: latLng,
             map: this.map,
             icon: {url: imgRestaurant}
             });
+
+            // let infowindow = new window.google.maps.InfoWindow({
+            //     content: '<div style="width:400px; height:400px" id="' + i + '"></div>'
+            // });
+            marker.addListener('click', () => {
+                this.getInfowindow(marker);
+                this.setStreetView(latLng);
+            });
         }
     }
 
-    nearbyRestaurants() {
+    nearbyRestaurants(draggedLocation = null) {
         let pyrmont = window.clientPosition;
+        if(draggedLocation != null) {
+            pyrmont = draggedLocation;
+        }
 
         let request = {
             location: pyrmont,
-            radius: '1000',
+            radius: '50000',
             type: ['restaurant']
             };
             
@@ -181,8 +233,20 @@ class GMap extends React.Component {
             position: place.geometry.location,
             map: this.map,
             title: place.name,
+            id: place.id,
             icon: imgRestaurant
             });
+        
+        marker.addListener('click', () => {
+            
+            if (this.setStreetView(place.geometry.location) === false) {
+               this.infowindow.setContent("No street view available!");
+               this.infowindow.open(this.map, marker);
+            } else {
+                this.getInfowindow(marker);
+            }; 
+            
+        });
     }
 
     permission(){
@@ -201,6 +265,7 @@ class GMap extends React.Component {
         return (
             <div>
                 <div id="map" ref={this.mapeRef} style={{width:"100%", height:"100%", backgroundColor:"grey", position: "absolute"}}></div>
+                {/* <div id="pano"></div> */}
                 <button className="c-button" value="My Location" />
                 <div className="new-restaurant-popup" style={{ display: this.state.newRestaurant.display ? "block":"none"}}>
                     <NewRestaurant style={{ top: this.state.newRestaurant.position[0], left: this.state.newRestaurant.position[1]}} display={this.state.newRestaurant.display} />
